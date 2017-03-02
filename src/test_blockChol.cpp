e09789db4ca266b33b57643aca78947be3ad5d8f
istinj <istinj@gmail.com>
2017-03-02 16:48:40 +0100

    pre CLion

---
+++ src/test_blockChol.cpp
@@ -0,0 +1,13 @@
+/*
+ * test_blockChol.cpp
+ *
+ *  Created on: 02/mar/2017
+ *      Author: istin
+ */
+#include <iostream>
+#include <Eigen/Core>
+
+int main(int argc, char **argv) {
+	Eigen::Vector3f m;
+	return 0;
+}
